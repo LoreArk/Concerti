3f1baa04f3c7239b3373f863c431bd1a15dd8fbf
LoreArk <lorepusa95@gmail.com>
2025-05-09 16:15:42 +0200

    Controller Package + empty IndexController

--- concertportal/src/main/java/com/concertportal/model/Administrator.java
+++ concertportal/src/main/java/com/concertportal/model/Administrator.java
@@ -18,10 +18,10 @@ public class Administrator {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
-  
+    
    // @Pattern(regexp = "[a-zA-Z0-9._-]{1,50}", message = "Caratteri Non Ammessi In Username")
     private String email;
-  
+    
    // @Pattern(regexp = "(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{6,50}", message = "Password Troppo Debole")
     private String password;
 }
